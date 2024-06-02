5f63dab054c8830684ac8b9ef45b4b7c8fd401a7
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-02 13:15:18 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -31,5 +31,24 @@ bMotorPower = false
 bM1 = false
 zeigeStatus()
 loops.everyInterval(400, function () {
-	
+    if (bMotorPower) {
+        basic.setLedColor(0x0000ff)
+    } else {
+        basic.setLedColor(basic.rgb(7, 0, 0))
+    }
+    if (radiosender.joystickQwiic() && !(bM1)) {
+        radio.fill_sendBuffer19()
+        radio.setBetriebsart(radio.radio_sendBuffer19(), radio.e0Betriebsart.p0)
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.p0, radio.eBufferOffset.b0_Motor, radiosender.joystickValues(radiosender.eJoystickValue.motor))
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.p0, radio.eBufferOffset.b1_Servo, radiosender.joystickValues(radiosender.eJoystickValue.servo))
+        radio.setaktiviert(radio.radio_sendBuffer19(), radio.e3aktiviert.m0, bMotorPower)
+        radio.setSchalter(radio.radio_sendBuffer19(), radio.e0Schalter.b0, input.buttonIsPressed(Button.B) && !(input.buttonIsPressed(Button.A)))
+        radio.sendData(radio.radio_sendBuffer19())
+    } else if (radiosender.joystickQwiic() && bM1) {
+        radio.fill_sendBuffer19()
+        radio.setBetriebsart(radio.radio_sendBuffer19(), radio.e0Betriebsart.p0)
+        radio.setByte(radio.radio_sendBuffer19(), radio.eBufferPointer.p1, radio.eBufferOffset.b0_Motor, radiosender.joystickValues(radiosender.eJoystickValue.motor))
+        radio.sendData(radio.radio_sendBuffer19())
+    }
+    basic.turnRgbLedOff()
 })
